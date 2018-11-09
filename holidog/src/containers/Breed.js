0315162be74396b2d91627c4a8d220897ee5decb
uditgulati <uditgulati0@gmail.com>
2018-11-09 20:27:41 +0530

    Multiple images fetching

--- holidog/src/containers/Breed.js
+++ holidog/src/containers/Breed.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 
 class Breed extends Component {
 	state = {
-		image: null
+		image: []
 	}
 
 	componentDidMount() {
 		//console.log(this.props.match.params)
 		const breedName = this.props.match.params.breed;
 		//console.log(`https://dog.ceo/api/breed/${breedName}/images/random`)
-		axios.get(`https://dog.ceo/api/breed/${breedName}/images/random`)
+		axios.get(`https://dog.ceo/api/breed/${breedName}/images/random/5`)
 			.then(res => {
 				console.log(res.data)
 				this.setState({image: res.data.message});
@@ -18,9 +18,13 @@ class Breed extends Component {
 	}
 
 	render() {
+		const images = this.state.image.map(img => {
+			return <img src={img} />;
+		})
+
 		return (
 			<div>
-				<img src={this.state.image} />
+				{images}
 			</div>
 		);
 	}
